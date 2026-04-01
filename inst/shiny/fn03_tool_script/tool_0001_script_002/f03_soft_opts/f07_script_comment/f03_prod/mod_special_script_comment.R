a82330e25846e10ec82bcf82be6d6249044c3f54
deliaspanigo <d.eliaspanigo@gmail.com>
2026-04-01 18:54:54 +0200

    Release: Version 0.3.6

--- inst/shiny/fn03_tool_script/tool_0001_script_002/f03_soft_opts/f07_script_comment/f03_prod/mod_special_script_comment.R
+++ inst/shiny/fn03_tool_script/tool_0001_script_002/f03_soft_opts/f07_script_comment/f03_prod/mod_special_script_comment.R
@@ -16,12 +16,28 @@ mod_special_script_comment_ui <- function(id) {
         navset_card_tab(
           title = "Editor de Reportes Quarto",
           nav_panel(
-            title = "Anova Original 01",
+            title = "Import and Control",
             mod_06_00_render_and_show_ui(id = ns("anova_01"))
           ),
           nav_panel(
-            title = "Anova Original 02",
+            title = "Full Test",
             mod_06_00_render_and_show_ui(id = ns("anova_02"))
+          ),
+          nav_panel(
+            title = "Model",
+            mod_06_00_render_and_show_ui(id = ns("anova_03"))
+          ),
+          nav_panel(
+            title = "Response Variable",
+            mod_06_00_render_and_show_ui(id = ns("anova_04"))
+          ),
+          nav_panel(
+            title = "Residuals",
+            mod_06_00_render_and_show_ui(id = ns("anova_05"))
+          ),
+          nav_panel(
+            title = "ASA",
+            mod_06_00_render_and_show_ui(id = ns("anova_06"))
           )
         )
     )
@@ -39,15 +55,44 @@ mod_special_script_comment_server <- function(id, folder_temp_path) {
 
     # Ejemplo con colores distintos configurados desde el Server
     mod_06_00_render_and_show_server(id = "anova_01",
-                            super_label = "Análisis de Varianza Principal",
+                            super_label = "Import and Control",
                             bg_color = "#e7f1ff", # Un azul muy suave
-                            input_file_path_qmd = reactive(file.path(base_folder(), "file01_anova_original.qmd")),
-                            output_file_path    = reactive(file.path(base_folder(), "file01_anova_original.html")))
+                            input_file_path_qmd = reactive(file.path(base_folder(), "file01_anova_import_and_control.qmd")),
+                            output_file_path    = reactive(file.path(base_folder(), "file01_anova_import_and_control.html")))
 
     mod_06_00_render_and_show_server(id = "anova_02",
-                            super_label = "Réplica Experimental 02",
+                            super_label = "Full Test",
                             bg_color = "#f0fff4", # Un verde muy suave
-                            input_file_path_qmd = reactive(file.path(base_folder(), "file01_anova_original_02.qmd")),
-                            output_file_path    = reactive(file.path(base_folder(), "file01_anova_original_02.html")))
+                            input_file_path_qmd = reactive(file.path(base_folder(), "file02_anova_full_test.qmd")),
+                            output_file_path    = reactive(file.path(base_folder(), "file02_anova_full_test.html")))
+
+    mod_06_00_render_and_show_server(id = "anova_03",
+                                     super_label = "Model",
+                                     bg_color = "#f0fff4", # Un verde muy suave
+                                     input_file_path_qmd = reactive(file.path(base_folder(), "file03_anova_model.qmd")),
+                                     output_file_path    = reactive(file.path(base_folder(), "file03_anova_model.html")))
+
+    mod_06_00_render_and_show_server(id = "anova_04",
+                                     super_label = "Response Variable",
+                                     bg_color = "#f0fff4", # Un verde muy suave
+                                     input_file_path_qmd = reactive(file.path(base_folder(), "file04_anova_descriptive_rv.qmd")),
+                                     output_file_path    = reactive(file.path(base_folder(), "file04_anova_descriptive_rv.html")))
+
+
+    mod_06_00_render_and_show_server(id = "anova_05",
+                                     super_label = "Residuals",
+                                     bg_color = "#f0fff4", # Un verde muy suave
+                                     input_file_path_qmd = reactive(file.path(base_folder(), "file05_anova_descriptive_residuals.qmd")),
+                                     output_file_path    = reactive(file.path(base_folder(), "file05_anova_descriptive_residuals.html")))
+
+
+
+    mod_06_00_render_and_show_server(id = "anova_06",
+                                     super_label = "ASA",
+                                     bg_color = "#f0fff4", # Un verde muy suave
+                                     input_file_path_qmd = reactive(file.path(base_folder(), "file06_anova_asa.qmd")),
+                                     output_file_path    = reactive(file.path(base_folder(), "file06_anova_asa.html")))
+
+
   })
 }
